7c22e22a824de0e119213798fbdacc9ce4dfe5da
SanPRLUDev <santiagodevh@gmail.com>
2025-02-19 20:26:35 -0500

    config: agregando el xml 3

--- mainProject/urls.py
+++ mainProject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic.base import TemplateView
 from .sitemaps import StaticViewSitemap
 
 sitemaps = {
@@ -30,4 +31,5 @@ urlpatterns = [
     path('api/',include('APIMain.urls')), 
     path('', include('market.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
